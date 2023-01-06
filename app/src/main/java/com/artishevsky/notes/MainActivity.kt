60f3d43aa4e82f94834916cb24aa17f52e489bdf
artishevskym <56644443+artishevskym@users.noreply.github.com>
2023-01-06 16:17:28 +0000

    Init Android Unit Test for MainActivity

--- app/src/main/java/com/artishevsky/notes/MainActivity.kt
+++ app/src/main/java/com/artishevsky/notes/MainActivity.kt
@@ -16,15 +16,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NotesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    NotesTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting("Android")
         }
     }
 }
